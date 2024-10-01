613a6da0a2ad437a8a1609d453862cb261d7cd8d
cleivas <clara.leivas@gmail.com>
2024-10-01 10:28:45 +0200

    added all files

---
+++ docs/index.34af1f0c.js
@@ -0,0 +1,2 @@
+console.log("hej"),document.querySelector("h1").style.color="hotpink";
+//# sourceMappingURL=index.34af1f0c.js.map
---
+++ src/main.js
@@ -0,0 +1,4 @@
+console.log('hej');
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'hotpink';
